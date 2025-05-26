c18f83ac7a25002c41af659384703253d1a85d57
Matheus Silveira Sergio <matheussilveirasergio@gmail.com>
2025-05-26 17:34:26 -0300

    docs: correção na parte de documentação

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -12,11 +12,10 @@ import 'package:usuario_teste_pratico/app/modules/auth/store/register_store.dart
 class AuthModule extends Module {
   @override
   void binds(Injector i) {
-    /// Registra o [AuthStore] como uma instância singleton no módulo.
+    /// Registra o [LoginStore] como uma instância singleton no módulo.
     ///
-    /// O [AuthStore] é responsável por gerenciar o estado e as operações
-    /// relacionadas à autenticação do usuário, como login, logout e verificação
-    /// de sessão.
+    /// O [LoginStore] é responsável por gerenciar o estado e as operações
+    /// relacionadas à autenticação do usuário, como login e registro.
     i.addSingleton(() => LoginStore());
     i.addSingleton(() => RegisterStore());
     i.addSingleton(() => LoginProvider(loginStore: i.get<LoginStore>()));
